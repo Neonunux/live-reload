7884cc246717fbccccb7062f3cff9681c2f3bde0
Regiloup <regis.leloup@colombbus.org>
2021-04-15 15:03:32 +0200

    initial commit

---
+++ src/DataCollector/RequestCollector.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
+
+class RequestCollector extends AbstractDataCollector
+{
+    public function collect(Request $request, Response $response, \Throwable $exception = null)
+    {
+        $this->data = [
+            'lastReload' => \Date('now'),
+            'acceptable_content_types' => $request->getAcceptableContentTypes(),
+        ];
+    }
+
+    public static function getTemplate(): ?string
+    {
+        return 'data_collector/template.html.twig';
+    }
+
+    public function getLastReload()
+    {
+        return $this->data['lastReload'];
+    }
+
+    public function getAcceptableContentTypes()
+    {
+        return $this->data['acceptable_content_types'];
+    }
+    // public function getName(): string
+    // {
+    //     return 'reload.data_collector.request';
+    // }
+}
